2e463124f781261fdf69d69d943f964b15862bc8
Savithu Induwara <savithuinduwara4@gmail.com>
2024-12-12 23:01:07 +0530

    Update config-v2.js

--- config-v2.js
+++ config-v2.js
@@ -1,6 +1,6 @@
-const GITHUB_AUTH_TOKEN = "null"
-const BOT_NUMBER = "null"
-const SESSION_ID = ""
+const GITHUB_AUTH_TOKEN = "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"
+const BOT_NUMBER = "94768268328"
+const SESSION_ID = "PRABATH-MD~3X5WHRqD#75bElJAC0vQXzc0pCp0uaUQnjCzQF7e-hf2CgZ5K4Yc"
 
 
 module.exports = {
